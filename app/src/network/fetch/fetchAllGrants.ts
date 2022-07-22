9a11334ad24039ef5437b04373a9c46e9bba7e9e
Spencer Kim <spencerk@alum.mit.edu>
2022-07-21 21:04:43 -0700

    add function

--- app/src/network/fetch/fetchAllGrants.ts
+++ app/src/network/fetch/fetchAllGrants.ts
@@ -1,10 +1,8 @@
 import { AnchorWallet } from '@solana/wallet-adapter-react'
-import Decimal from 'decimal.js-light'
 
 import { connection } from '../connection'
 import { getGrantProgram } from '../getGrantProgram'
-import { ContentSHA256 } from '../types/ContentSHA256'
-import { Grant } from '../types/Grant'
+import { formatGrant, Grant } from '../types/Grant'
 
 interface Args {
   wallet: AnchorWallet
@@ -13,12 +11,5 @@ interface Args {
 export const fetchAllGrants = async ({ wallet }: Args): Promise<Grant[]> => {
   const program = getGrantProgram(wallet, connection)
   const grants = await program.account.grant.all()
-  return grants.map((g) => {
-    const grant: Grant = {
-      publicKey: g.publicKey,
-      contentSha256: g.account.contentSha256 as ContentSHA256,
-      initialAmountLamports: new Decimal(g.account.initialAmount.toString()),
-    }
-    return grant
-  })
+  return grants.map((g) => formatGrant(g.publicKey, g.account))
 }
--- app/src/network/fetch/fetchGrant.ts
+++ app/src/network/fetch/fetchGrant.ts
@@ -1,11 +1,9 @@
 import { AnchorWallet } from '@solana/wallet-adapter-react'
 import { PublicKey } from '@solana/web3.js'
-import Decimal from 'decimal.js-light'
 
 import { connection } from '../connection'
 import { getGrantProgram } from '../getGrantProgram'
-import { ContentSHA256 } from '../types/ContentSHA256'
-import { Grant } from '../types/Grant'
+import { formatGrant } from '../types/Grant'
 
 interface Args {
   grantPubkey: PublicKey
@@ -15,12 +13,5 @@ interface Args {
 export const fetchGrant = async ({ grantPubkey, wallet }: Args) => {
   const program = getGrantProgram(wallet, connection)
   const grantAccount = await program.account.grant.fetch(grantPubkey)
-
-  const grant: Grant = {
-    publicKey: grantPubkey,
-    contentSha256: grantAccount.contentSha256 as ContentSHA256,
-    initialAmountLamports: new Decimal(grantAccount.initialAmount.toString()),
-  }
-
-  return grant
+  return formatGrant(grantPubkey, grantAccount)
 }
