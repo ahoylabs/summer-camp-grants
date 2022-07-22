040dc6b55347e1f6bed7c38f04b098a8736922c6
Spencer Kim <spencerk@alum.mit.edu>
2022-07-21 23:11:36 -0700

    fix

--- app/src/network/types/models/Grant.ts
+++ app/src/network/types/models/Grant.ts
@@ -1,4 +1,5 @@
 import { PublicKey } from '@solana/web3.js'
+import dayjs, { Dayjs } from 'dayjs'
 import Decimal from 'decimal.js-light'
 
 import { AhoyGrants } from '../../../../../contract/target/types/ahoy_grants'
@@ -7,6 +8,7 @@ import { TypedProgram } from '../typedProgram'
 
 export interface Grant {
   contentSha256: ContentSHA256
+  createdAt: Dayjs
   initialAmountLamports: Decimal
   publicKey: PublicKey
 }
@@ -20,5 +22,6 @@ export const formatGrant = (pubkey: PublicKey, account: AnchorGrant): Grant => {
     publicKey: pubkey,
     contentSha256: account.contentSha256 as ContentSHA256,
     initialAmountLamports: new Decimal(account.initialAmount.toString()),
+    createdAt: dayjs.unix(account.createdAt.toNumber()),
   }
 }
