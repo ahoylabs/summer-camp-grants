9056e656dea99dfefa2b002437c46e98eaa8da9b
Spencer Kim <spencerk@alum.mit.edu>
2022-07-26 00:16:25 -0700

    read usdc mint from env var in getAssociatedTokenAddresss

--- app/src/network/getUSDCAssociatedTokenAddress.ts
+++ app/src/network/getUSDCAssociatedTokenAddress.ts
@@ -2,15 +2,26 @@ import { utils } from '@project-serum/anchor'
 import { getAssociatedTokenAddress } from '@solana/spl-token'
 import { PublicKey } from '@solana/web3.js'
 
+import {
+  NEXT_PUBLIC_SOLANA_CLUSTER,
+  NEXT_PUBLIC_USDC_MINT_ADDR,
+} from './../__generated__/_env'
 import { localFaucetProgram } from './getFaucetProgram'
 
 export const getUSDCAssociatedTokenAddress = async (
   owner: PublicKey,
 ): Promise<PublicKey> => {
-  const [mintPda] = await PublicKey.findProgramAddress(
-    [Buffer.from(utils.bytes.utf8.encode('faucet-mint'))],
-    localFaucetProgram, // hardcode this to USDC mint address for prod
-  )
+  if (NEXT_PUBLIC_SOLANA_CLUSTER !== 'mainnet') {
+    // use the custom faucet program
+    const [mintPda] = await PublicKey.findProgramAddress(
+      [Buffer.from(utils.bytes.utf8.encode('faucet-mint'))],
+      localFaucetProgram,
+    )
+    return await getAssociatedTokenAddress(mintPda, owner)
+  }
 
-  return await getAssociatedTokenAddress(mintPda, owner)
+  return await getAssociatedTokenAddress(
+    new PublicKey(NEXT_PUBLIC_USDC_MINT_ADDR),
+    owner,
+  )
 }
